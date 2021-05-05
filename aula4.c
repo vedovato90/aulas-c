06bb7226e2c7cc4245872e3eef33722e9ec07226
Gabriel Vedovato <82051730+vedovato90@users.noreply.github.com>
2021-05-04 21:36:42 -0300

    Create aula4.c

---
+++ aula4.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+#include <conio.h>
+int main(void)
+
+float notas [5] = {7, 8, 9.5, 9.3, 5,2}
+// declarando e inicializando o vetor notas
+
+printf ("Exibindo os valores do vetor \n\n");
+printf ("notas [0] = %.1f\n", notas [0]);
+printf ("notas [1] = %.1f\n", notas [1]);
+printf ("notas [2] = %.1f\n", notas [2]);
+printf ("notas [3] = %.1f\n", notas [3]);
+printf ("notas [4] = %.1f\n", notas [4]);
+
+getch();
+return 0;
